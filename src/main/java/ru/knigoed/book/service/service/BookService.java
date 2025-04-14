b73bae57038eabc3e758aa1d18476cc69def6b00
asharykin <aleksandr-sharykin@mail.ru>
2025-04-14 20:02:58 +0300

    Refactoring

--- src/main/java/ru/knigoed/book/service/service/BookService.java
+++ src/main/java/ru/knigoed/book/service/service/BookService.java
@@ -6,6 +6,7 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ru.knigoed.book.service.dto.BookDTO;
 import ru.knigoed.book.service.entity.Book;
 import ru.knigoed.book.service.mapper.BookMapper;
@@ -33,12 +34,14 @@ public class BookService {
         return new PageImpl<>(bookDTOs, pageable, bookPage.getTotalElements());
     }
 
+    @Transactional
     public BookDTO createBook(BookDTO bookDTO) {
         Book book = bookMapper.toEntity(bookDTO);
         Book savedBook = bookRepository.save(book);
         return bookMapper.toDTO(savedBook);
     }
 
+    @Transactional
     public BookDTO updateBook(Long id, BookDTO bookDTO) {
         Book book = bookMapper.toEntity(bookDTO);
         book.setId(id);
@@ -46,6 +49,7 @@ public class BookService {
         return bookMapper.toDTO(updatedBook);
     }
 
+    @Transactional
     public void deleteBook(Long id) {
         bookRepository.deleteById(id);
     }
